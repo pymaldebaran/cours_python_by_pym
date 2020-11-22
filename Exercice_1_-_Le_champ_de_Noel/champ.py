c82ca4eadb28a7ced4a65eb2685f10de0d00bccd
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2020-11-22 13:28:53 +0100

    Exercice 1: Maintenant il y a plusieurs arbres !

--- Exercice_1_-_Le_champ_de_Noel/champ.py
+++ Exercice_1_-_Le_champ_de_Noel/champ.py
@@ -1,24 +1,45 @@
+#!/usr/bin/env python3
+
 from random import randrange
 
-COLUMN = 10  # Sans les bordures (+2)
-LINE = 5  # Sans les bordures (+2)
+def main():
+    NB_COLUMN = 10  # Sans les bordures (+2)
+    NB_LINES = 5  # Sans les bordures (+2)
+    VERTICAL_EDGE = (NB_COLUMN + 2) * '#'
+    NB_TREES = 5
 
-RAND_COLUMN = randrange(1, COLUMN+1) 
-RAND_LINE = randrange(1, LINE+1)
-BORDURE_VERTICALE = (COLUMN + 2) * '#'
+    LINE_WO_TREE = '#' + ' ' * (NB_COLUMN) + '#'
+    line_w_trees = ['#'] + [' '] * (NB_COLUMN) + ['#']
 
-print(RAND_COLUMN)
-print(RAND_LINE)
+    TREE_COORD = []
+    X_COORD = []   
+    Y_COORD = []
 
-ligne_sans_arbre = '#' + ' ' * (COLUMN) + '#'
-ligne_avec_arbres = '#' + ' ' * (COLUMN-1) + '#'
+    for tree in range(NB_TREES):
+        TREE_COORD.append((randrange(1, NB_COLUMN + 1),randrange(1, NB_LINES + 1)))
+        X_COORD.append(TREE_COORD[tree][0])
+        Y_COORD.append(TREE_COORD[tree][1])
 
-print(BORDURE_VERTICALE)
-for i in range(1, LINE + 1):
+    print(TREE_COORD,"\n")
+    print(X_COORD)
+    print(Y_COORD, "\n")   
+
+
+    print(VERTICAL_EDGE)
+    for i in range(1, NB_LINES + 1):
+        if i in Y_COORD:
+            for (x,y) in TREE_COORD:
+                if i == y:
+                    line_w_trees.remove(' ')
+                    line_w_trees.insert(x,'o')
+            print(''.join(line_w_trees))
+            for o in line_w_trees:
+                if o == 'o':
+                    line_w_trees.remove('o')
+                    line_w_trees.insert(x,' ')
+        else:
+            print(LINE_WO_TREE)
+    print(VERTICAL_EDGE)   
     
-    if i == RAND_LINE :             
-        print (ligne_avec_arbres[:RAND_COLUMN] 
-               + 'o' + ligne_avec_arbres[RAND_COLUMN:])
-    else:
-        print(ligne_sans_arbre)
-print(BORDURE_VERTICALE)
+if __name__ == '__main__':
+    main()
