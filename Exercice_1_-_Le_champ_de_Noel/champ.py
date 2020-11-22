c28aaee37b4a3fb021c8a6f284c56bb5e5f97c38
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2020-11-22 12:27:31 +0100

    Exercice1: Premier jet.
    Pas optimisé, avec un seul arbre.

--- Exercice_1_-_Le_champ_de_Noel/champ.py
+++ Exercice_1_-_Le_champ_de_Noel/champ.py
@@ -1,1 +1,22 @@
-# Met ton code ici
+from random import *
+
+column = 20 #sans les bordures (+2)
+line = 9 #sans les bordures (+2)
+
+rand_column = randrange(1,column+1)
+rand_line = randrange(1,line+1)
+
+list_sans_arbre = ['#'] + [' ']*(column) + ['#']
+list_arbre = ['#'] + [' ']*(column-1) + ['#']
+
+
+print((column+2)*"#")
+for i in range(line):
+    
+    if i != rand_line :
+        print ("".join(list_sans_arbre))
+    else:
+        
+        list_arbre.insert(rand_column,'o')
+        print ("".join(list_arbre))
+print((column+2)*"#")
