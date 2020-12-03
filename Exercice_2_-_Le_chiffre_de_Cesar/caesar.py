08e3f429a8ce3af6bea3020c80d3e2685c314dd0
CylonProgSpace <71029029+CylonProgSpace@users.noreply.github.com>
2020-12-03 21:30:17 +0100

    Exo2 (#7)
    Solve exo 2
    
    * implementation of do_cipher function
    * implementation of do_decipher function
    
    Co-authored-by: Thibault <thibault.delrieu@obspm.fr>
    Co-authored-by: Pierre-Yves Martin <pym.aldebaran@gmail.com>

--- Exercice_2_-_Le_chiffre_de_Cesar/caesar.py
+++ Exercice_2_-_Le_chiffre_de_Cesar/caesar.py
@@ -3,18 +3,77 @@
 Solution de l'exercice 2 tel que définit dans
 [l'énoncé de l'exercice 2](README.md).
 """
-
+import string
+import logging
+import sys
 from typing import Dict
 
+class Colors:
+    """Colored prints
+    """
+
+    NC = "\033[0m"
+    RED = "\033[31m"
+
+
+def logging_print(message: str, type_loging: logging):
+    """create logs
+    """
+    type_loging(message)
+    
+def create_transtab(key: int) -> str:
+    """return the transtab
+
+    the transtab is a dict which can bu used with translate method in order to transform a string into a cypher string
+    """
+    TRUE_ALPHABET = string.ascii_uppercase
+    Z_UPPER_ASCII = 90
+    A_UPPER_ASCII = 65
+    AJUSTMENT = 1  # make ajustment in order to take the last or the first letter
+    if A_UPPER_ASCII + key >= A_UPPER_ASCII:
+        begining_cypher_alphabet = A_UPPER_ASCII + key
+    else:
+        begining_cypher_alphabet = Z_UPPER_ASCII + key + AJUSTMENT
+    if Z_UPPER_ASCII + key <= Z_UPPER_ASCII:
+        ending_cypher_alphabet = Z_UPPER_ASCII + key
+    else:
+        ending_cypher_alphabet = A_UPPER_ASCII + key - AJUSTMENT
+
+    cypher_alphabet_begin_to_Z = list(
+        map(chr, range(begining_cypher_alphabet, Z_UPPER_ASCII + AJUSTMENT))
+    )
+    cypher_alphabet_Z_to_ending = list(
+        map(chr, range(A_UPPER_ASCII, ending_cypher_alphabet + AJUSTMENT))
+    )
+    cypher_alphabet = "".join(cypher_alphabet_begin_to_Z) + "".join(
+        cypher_alphabet_Z_to_ending
+    )
+
+    transtab = str.maketrans(TRUE_ALPHABET, cypher_alphabet)
+    return transtab
 
 def do_cipher(plain_text: str, key: int) -> str:
-    # Met ton code ici
-    return "Il faudrait retourner un vrai truc ;)"
+    """Cipher a string object with the given key"""
+    if key > 26 or key < -26:
+        logging_print(
+            f"{Colors.RED}Need to have a key between -26 and 26{Colors.NC}",
+            logging.error,
+        )
+        sys.exit()
 
+    plain_text_upper = plain_text.upper()
+    transtab = create_transtab(key)
+    cypher_text = plain_text_upper.translate(transtab)
 
+    return cypher_text
+
+  
 def do_decipher(cipher_text: str, key: int) -> str:
-    # Met ton code ici
-    return "Il faudrait retourner un vrai truc ;)"
+    """Decipher a string object with the given key"""
+    counter_key = -key
+    transtab = create_transtab(counter_key)
+    decipher_text = cipher_text.translate(transtab)
+    return decipher_text
 
 
 def exercise_2_1():
